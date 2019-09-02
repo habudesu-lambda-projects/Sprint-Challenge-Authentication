4b37c609e3f14f98272af741935b79585fddb693
Ken Ruf <ruf.kenichi@gmail.com>
2019-09-01 20:37:26 -0500

    login

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -1,9 +1,14 @@
 const db = require('../database/dbConfig.js')
 
 module.exports = {
-    addUser
+    addUser,
+    findUserBy
 }
 
 function addUser(user) {
     return db('users').insert(user) 
+}
+
+function findUserBy(filter) {
+    return db('users').where(filter).first()
 }
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,8 +17,33 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res) => {
+  const {username, password} = req.body
+  try {
+    const user = await User.findUserBy({username})
+    console.log(user)
+    if(user && bcrypt.compareSync(password, user.password)) {
+      const token = createToken(user)
+      res.status(200).json({message: "Login Successful", token})
+    } else {
+      res.status(400).json({message: "Invalid Credentials"})
+    }
+  }
+  catch(error) {
+    res.status(500).json({message: "Could Not Login", error: error})
+  }
 });
+
+function createToken(user) {
+  const payload = {
+    subject: "user",
+    username: user.username
+  }
+  const secret = "blah-blah-blah"
+  const options = {
+    expiresIn: '1h'
+  }
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
