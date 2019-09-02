43d1487834c1d072c15bce6641536a1b62d03c9e
Ken Ruf <ruf.kenichi@gmail.com>
2019-09-01 20:21:32 -0500

    register use

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-router.post('/register', (req, res) => {
-  // implement registration
+const User = require('./auth-model.js')
+
+router.post('/register', async (req, res) => {
+  const user = req.body
+  const hashedPW = bcrypt.hashSync(user.password, 10)
+  user.password = hashedPW
+  try {
+    const newUser = await User.addUser(user)
+    res.status(201).json(user)
+  }
+  catch(error) {
+    res.status(500).json({message: "Could Not Register User", error: error})
+  }
 });
 
 router.post('/login', (req, res) => {
